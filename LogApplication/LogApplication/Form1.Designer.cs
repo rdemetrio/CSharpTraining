ef086d7b9e7e06ac6718e52b49560ba0fa947f43
Rogelea Demetrio <rdemetrio@meridianautoparts.com>
2013-10-08 15:41:01 -0700

    LogAppFinal

---
+++ LogApplication/LogApplication/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace LogApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.tbInputString = new System.Windows.Forms.TextBox();
+            this.btnLogString = new System.Windows.Forms.Button();
+            this.btnDeleteString = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.listboxInput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Location = new System.Drawing.Point(13, 15);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(74, 13);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Enter a String:";
+            // 
+            // tbInputString
+            // 
+            this.tbInputString.Location = new System.Drawing.Point(93, 12);
+            this.tbInputString.Name = "tbInputString";
+            this.tbInputString.Size = new System.Drawing.Size(289, 20);
+            this.tbInputString.TabIndex = 1;
+            // 
+            // btnLogString
+            // 
+            this.btnLogString.Location = new System.Drawing.Point(407, 10);
+            this.btnLogString.Name = "btnLogString";
+            this.btnLogString.Size = new System.Drawing.Size(97, 23);
+            this.btnLogString.TabIndex = 2;
+            this.btnLogString.Text = "Log";
+            this.btnLogString.UseVisualStyleBackColor = true;
+            this.btnLogString.Click += new System.EventHandler(this.btnLogString_Click);
+            // 
+            // btnDeleteString
+            // 
+            this.btnDeleteString.Location = new System.Drawing.Point(407, 133);
+            this.btnDeleteString.Name = "btnDeleteString";
+            this.btnDeleteString.Size = new System.Drawing.Size(97, 23);
+            this.btnDeleteString.TabIndex = 4;
+            this.btnDeleteString.Text = "Delete";
+            this.btnDeleteString.UseVisualStyleBackColor = true;
+            this.btnDeleteString.Click += new System.EventHandler(this.btnDeleteString_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(407, 53);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(97, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load / Refresh";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(407, 216);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(97, 23);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // listboxInput
+            // 
+            this.listboxInput.FormattingEnabled = true;
+            this.listboxInput.Location = new System.Drawing.Point(16, 53);
+            this.listboxInput.Name = "listboxInput";
+            this.listboxInput.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.listboxInput.Size = new System.Drawing.Size(366, 186);
+            this.listboxInput.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 257);
+            this.Controls.Add(this.listboxInput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnDeleteString);
+            this.Controls.Add(this.btnLogString);
+            this.Controls.Add(this.tbInputString);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logging Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox tbInputString;
+        private System.Windows.Forms.Button btnLogString;
+        private System.Windows.Forms.Button btnDeleteString;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox listboxInput;
+    }
+}
+
---
+++ LogApplication/LogApplication/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LogApplication
+{
+    public partial class Form1 : Form
+    {
+        string input, format, stamp, filename;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listboxInput.Visible = false;
+
+            DateTime logTime = DateTime.Now;
+            format = "yyyy-MMM-d HH:mm:ss ";
+            stamp = logTime.ToString(format);
+            filename = @"C:\Users\rdemetrio\Documents\GitHub\CSharpTraining\FileLog.txt";
+        }
+
+        //Load and display log file
+        private void LoadAndDisplayLogFile()
+        {
+            //check to see if the file is created
+            if (File.Exists(filename))
+            {
+                //Clear the list box contents before reloading
+                listboxInput.Items.Clear();
+                
+                //string line;
+                using (StreamReader sR = new StreamReader(filename))
+                {
+                    //Read each line out of the file and populate our listbox
+                    string curLine;
+                    while ((curLine = sR.ReadLine()) != null)
+                    {
+                        //As long as we don't read a null value back (end of the file),
+                        //add each line from the source file to the list box
+                        listboxInput.Items.Add(curLine);
+                    }
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException(string.Format("Source file \"{0}\" not found.", filename));
+            }
+        }
+        
+        //log input 
+        private void btnLogString_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                input = tbInputString.Text.Trim();
+
+                if (input != string.Empty)
+                {
+                    //Set the FileMode value that we're going to use for the file stream,
+                    //based on whether the file exists or not
+                    FileMode fMode;
+                    if (File.Exists(filename))
+                    {
+                        //The file exists, so append to it
+                        fMode = FileMode.Append;
+                    }
+                    else
+                    {
+                        //The file doesn't exist, so create a new one
+                        fMode = FileMode.Create;
+                    }
+
+                    //Create the stream to the output file
+                    using (FileStream outputFile = new FileStream(filename, fMode))
+                    //Create a writer to write to the output stream
+                    using (TextWriter writer = new StreamWriter(outputFile))
+                    {
+                        writer.WriteLine(string.Format("{0} {1}", input, stamp));
+                    }
+
+                    LoadAndDisplayLogFile();
+                    
+                    tbInputString.Focus();
+                }
+                else
+                {
+                    //throw new Exception();
+                    throw new ApplicationException("No log entry provided.");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Input field cannot be empty", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            tbInputString.Clear();
+           
+        }
+
+        //load content from the file
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            listboxInput.Visible = true;
+            try
+            {
+                LoadAndDisplayLogFile();
+            }
+            catch(Exception ex)
+            {
+                //MessageBox.Show("File does not exist.", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show(string.Format("**ERROR** - {0}", ex.Message), "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //delete item from list
+        private void btnDeleteString_Click(object sender, EventArgs e)
+        {
+            //int selectedIndex = listboxInput.SelectedIndex;
+
+            try
+            {
+                int selectedIndex = listboxInput.SelectedIndex;
+                if (selectedIndex == -1)
+                    throw new ApplicationException("No item selected to delete.");
+                
+                //lines.RemoveAt(selectedIndex);
+                listboxInput.Items.RemoveAt(selectedIndex);
+
+                //We are overwriting the existing output file with the list box contents
+                using (FileStream outputFile = new FileStream(filename, FileMode.Create))
+                using (TextWriter writer = new StreamWriter(outputFile))
+                {
+                    foreach (string line in listboxInput.Items)
+                        writer.WriteLine(line);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            MessageBox.Show("Item deleted, file has been updated.", "Important Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //exits application 
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?", "End Application?", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            switch (result)
+            {
+                case DialogResult.OK:
+                    {
+                        Application.Exit();
+                        break;
+                    }
+                case DialogResult.Cancel:
+                    {
+                        this.Text = "[Cancel]";
+                        break;
+                    }
+            }
+        }
+    }
+}
